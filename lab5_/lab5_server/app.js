32af59430e15eea472e61f705f93d12e2ae7e227
cchala <79107461+cchala@users.noreply.github.com>
2021-07-07 20:32:46 -0500

    lab6

--- lab5_/lab5_server/app.js
+++ lab5_/lab5_server/app.js
@@ -1,13 +1,16 @@
 const express=require('express')
 const bookRouters=require('./routes/bookRoutes')
 const cors=require('cors');
+const authRouter=require('./routes/userRouter')
 const { static } = require('express');
+
 const app=express();
 
 const urlencodedParser = express.urlencoded({ extended: false })
 app.use(cors())
 app.use(express.json())
 
+app.use(authRouter)//login/
 app.use(bookRouters)
 
 
--- lab5_/lab5_server/routes/bookRoutes.js
+++ lab5_/lab5_server/routes/bookRoutes.js
@@ -2,13 +2,14 @@ const express = require('express')
 const books = require('../models/book')
 const bookContr = require('../controllers/bookController')
 const router = express.Router()
+const authUserJwtoken=require('../controllers/userController')
 
 
 router.get('/books', bookContr.findAllBooks)
-router.get('/books/:bookID', bookContr.findBookByi)
-router.post('/books', bookContr.savedBook)
-router.put('/books/:bookID', bookContr.updatedBooks)
-router.delete('/books/:bookID', bookContr.deleteByID)
+router.get('/books/:bookID',authUserJwtoken.authorizationAdmin, bookContr.findBookByi)
+router.post('/books' ,bookContr.savedBook)
+router.put('/books/:bookID',authUserJwtoken.authorizationAdmin, bookContr.updatedBooks)
+router.delete('/books/:bookID', authUserJwtoken.authorizationAdmin,bookContr.deleteByID)
 
 
 module.exports = router;
